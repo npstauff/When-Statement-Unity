fbb1d842ac87037d1e80d4d181af377ff1de0d0a
nix3220 <80929841+nix3220@users.noreply.github.com>
2021-12-05 21:36:43 -0500

    Add files via upload

---
+++ WhenExample.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhenExample : MonoBehaviour
+{
+    //Floats to trigger the when statement
+    float a = 3;
+    float b;
+
+    
+    void Start()
+    {
+
+        //debug a message the first moment that b becomes greater than a
+        When when = new When(() => b > a,
+            () =>
+            {
+                Debug.Log("When Has Been Triggered");
+            }
+            );
+    }
+
+    private void Update()
+    {
+        //increase b by deltatime in order for it become greater than a
+        b += Time.deltaTime;
+        
+    }
+
+
+}
